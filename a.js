d99a586162cc46a7c42db9118dcef203cab79a8f
losemankim <totory1999@naver.com>
2023-06-05 10:52:50 +0900

    test

--- a.js
+++ a.js
@@ -8,33 +8,33 @@
   
     var container = document.querySelector(".centered");
   
-    function displayStrings() {
-      if (strings.length > 0) {
-        var string = strings.shift();
-        var paragraph = document.createElement("p");
-        container.appendChild(paragraph);
+    function displayStrings() { // 함수 선언문
+      if (strings.length > 0) { // 배열의 길이가 0보다 크면
+        var string = strings.shift(); // 배열의 첫번째 요소를 제거하고 반환
+        var paragraph = document.createElement("p"); // p 요소 생성
+        container.appendChild(paragraph); // p 요소를 container에 추가
   
-        var charIndex = 0;
-        var typingEffect = setInterval(function() {
-          if (charIndex < string.length) {
-            paragraph.textContent += string.charAt(charIndex);
-            charIndex++;
-          } else {
-            clearInterval(typingEffect);
-            setTimeout(function() {
-              paragraph.style.animation = "fade-out 2s ease forwards";
-              setTimeout(function() {
-                container.removeChild(paragraph);
-                displayStrings();
+        var charIndex = 0; // 문자 인덱스 초기화
+        var typingEffect = setInterval(function() { // 함수 표현식
+          if (charIndex < string.length) { // 문자 인덱스가 문자열의 길이보다 작으면
+            paragraph.textContent += string.charAt(charIndex); // p 요소에 문자열의 문자를 추가
+            charIndex++;// 문자 인덱스 증가
+          } else {// 문자 인덱스가 문자열의 길이보다 크거나 같으면
+            clearInterval(typingEffect);// 타이핑 효과 제거
+            setTimeout(function() {// 2초 후에
+              paragraph.style.animation = "fade-out 2s ease forwards";// p 요소에 fade-out 애니메이션 효과 적용
+              setTimeout(function() {// 2초 후에
+                container.removeChild(paragraph);// p 요소를 container에서 제거
+                displayStrings();// displayStrings 함수 호출 재귀
               }, 2000);
             }, 2000);
           }
         }, 100);
       } else {
-        clearInterval(interval);
+        clearInterval(interval);// interval 제거
       }
     }
   
-    var interval = setInterval(displayStrings, 2000);
+    var interval = setInterval(displayStrings, 2000); // 2초마다 displayStrings 함수 호출
   });
   
